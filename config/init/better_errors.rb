274a6ce2de59e0b23d5440d07d944bf29df7203a
Christian Hellsten <christian@aktagon.com>
2014-06-15 11:21:14 +0000

    Fixes...

--- config/init/better_errors.rb
+++ config/init/better_errors.rb
@@ -1,6 +1,5 @@
-require "better_errors"
-
 if App.environment == :development
+  require "better_errors"
   App.use BetterErrors::Middleware
   BetterErrors.application_root = __dir__
   BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
